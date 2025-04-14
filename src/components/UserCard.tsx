79e13a805cc276a45a6bc6baecefee0b39af8e1a
Raylux <alessandro.phan.12@gmail.com>
2025-04-14 09:47:43 +0200

    feat: added Button and UserCard components

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,14 @@
+
+type UserCardProps = {
+    name: string;
+    email: string;
+}
+
+export const UserCard: React.FC<UserCardProps> = ({ name, email }: UserCardProps) => {
+    return (
+        <div style={{border: "1px solid #ccc", padding: "1rem"}}>
+            <h3>{name}</h3>
+            <p>{email}</p>
+        </div>
+    );
+};
